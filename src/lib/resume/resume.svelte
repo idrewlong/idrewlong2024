46f396e2709b591a1e38e753ee745071e3208cde
Richard Andrew Long <id-dev@Richards-MacBook-Air.local>
2024-03-10 21:23:36 -0500

    resume and bio updates

--- src/lib/resume/resume.svelte
+++ src/lib/resume/resume.svelte
@@ -4,7 +4,7 @@
           <h1 class="font-bold text-center text-xl md:text-3xl">Web Developer | Software Developer</h1>
         </div>
         <div class="flex justify-center">
-                <h2 class="font-semibold text-center text-xs md:text-xl">Front-End Developer | Back-End Developer </h2>
+                <h2 class="font-semibold text-center text-xs md:text-xl">Front-End Development | UI/UX | SEO | API's </h2>
         </div>
         <div class="flex justify-center">
                 <p class="text-center"><a class="hover:text-orange-400 duration-300" href="mailto:idrewlong@gmail.com">idrewlong@gmail.com</a> | Nashville, TN</p>
@@ -14,9 +14,9 @@
 <!-- About -->
 <section class="max-w-7xl mx-auto rounded-3xl shadow-lg p-5 my-8 bg-white grid grid-cols-1">
         <div class="flex flex-col max-w-6xl mx-auto justify-center items-center">
-                <p><b>Tech-savvy Professional & Developer</b> specializing in marketing design, translating brand guidelines into impactful concepts. Proficient in content creation and web development, with a keen eye for design aesthetics and user experience. Adept at collaborating and leading with cross-functional teams and clients to deliver high-quality digital solutions. </p>
+                <p><b>Tech-savvy Professional & Developer</b> specializing in front-end development, utilizing tools such as, React.js, Svelte.js, and Tailwind CSS to create impactful websites. Proficient in object-oriented programming and web development, with a keen eye for design aesthetics and user experience. Adept at collaborating and leading with cross-functional teams and clients to deliver high-quality digital solutions. </p>
                 <ul class="p-4">
-                        <li> - <b>A pragmatic leader,</b> driving successful marketing strategies and providing engaging user experiences on the web; instrumental in crafting dynamic landing pages, email templates, and digital assets to elevate user experience. </li>
+                        <li> - <b>A pragmatic leader,</b> driving successful development strategies and providing engaging user experiences on the web; instrumental in crafting dynamic landing pages, unique components, and digital assets to elevate user experience. </li>
                         <li class="mt-2">
                          - <b>Proven track record </b> of optimizing brand recognition and delivering high-performance websites. Devoted to ensuring robust, visually appealing experiences via strategic layout optimization, navigation refinement, and responsive design. 
                         </li>
@@ -31,20 +31,23 @@
         </div>
 
         <div class="grid grid-cols-1 md:grid-cols-3 ">
-                <ul>
-                        <li>test</li>
-                        <li>test</li>
-                        <li>test</li>
+                <ul class="space-y-2">
+                        <li> - Web Development</li>
+                        <li> - Collaborative Leadership</li>
+                        <li> - UI/UX Optimization</li>
+                        <li class="pb-2 md:mb-0"> - Project Management</li>
                 </ul>
-                <ul>
-                        <li>test</li>
-                        <li>test</li>
-                        <li>test</li>
+                <ul class="space-y-2">
+                        <li> - Content Development & Support</li>
+                        <li> - Data Structures & Algorithms</li>
+                        <li> - Business Development & Marketing</li>
+                        <li class="pb-2 md:mb-0"> - Interactive Web Components</li>
                 </ul>
-                <ul>
-                        <li>test</li>
-                        <li>test</li>
-                        <li>test</li>
+                <ul class="space-y-2">
+                        <li> - Develop API's</li>
+                        <li> - Object-Oriented Programming</li>
+                        <li> - Cross-Functional Collaboration</li>
+                        <li> - Data-Driven Decision Making</li>
                 </ul>
         </div>
 </section>
@@ -59,19 +62,19 @@
                 <div class="my-5">
                         <h3 class="text-xl font-bold ">Thompson Machinery</h3>
                         <span>Marketing Coordinator</span>
-                        <p class="text-gray-500">Led comprehensive marketing and sales initiatives for both the sales and SITECH divisions. Established and nurtured strong relationships with sales managers and coordinators, fostering a culture of collaborative leadership. Worked closely with cross-functional teams to implement Caterpillar standardized marketing strategies. Utilized in-depth analysis of client data to refine and accurately define Ideal Customer Profiles (ICPs) tailored to specific regions. Designed and developed comprehensive landing pages utilizing HTML, CSS and JavaScript. </p>
+                        <p class="text-gray-500">Managed and led comprehensive marketing initiatives for both the sales and SITECH divisions. Established and nurtured strong relationships with sales managers and coordinators, fostering a culture of collaborative leadership. <br>Worked closely with cross-functional teams to implement Caterpillar standardized marketing strategies. Utilized in-depth analysis of client data to refine and accurately define Ideal Customer Profiles (ICPs) tailored to specific regions. Designed and developed comprehensive landing pages utilizing HTML, CSS and JavaScript. </p>
                 </div>
                 <!-- Info #3 -->
                 <div class="my-5">
                         <h3 class="text-xl font-bold">Finder's Guide</h3>
                         <span>Owner | Web Developer | Marketing Manager</span>
-                        <p class="text-gray-500">Orchestrate client projects, specializing in Marketing and Branding strategies. Maintain direct sales and cultivated strong client relationships. Collaborated with cross-functional remote teams to implement innovative social media and marketing strategies. Established branding strategies for prominent companies, including 44 Construction, Fat8 Media, Leflore Steel, TRKBX, and Provinyl Studio. Utilized frameworks such as React.js, Next.js, and Svelte to craft seamless interfaces with server-side rendering (SSR) for optimal website performance.</p>
+                        <p class="text-gray-500">Orchestrate client projects, specializing in Marketing and Branding strategies. Maintain direct sales and cultivated strong client relationships. Collaborated with cross-functional remote teams to implement innovative social media and marketing strategies. Established branding strategies for prominent companies, including 44 Construction, Fat8 Media, Leflore Steel, TRKBX, and Provinyl Studio. Utilized frameworks such as React.js, Next.js, and Svelte to craft seamless websites with server-side rendering (SSR) for optimal website performance.</p>
                 </div>
                 <!-- Info #4 -->
                 <div class="my-5">
                         <h3 class="text-xl font-bold">Core Logic</h3>
                         <span>Customer Support Specialist</span>
-                        <p class="text-gray-500">Selected to identify and meet customer needs and assist customers in utilizing specific features in the software, including DASH, Pro-Assist, Luxor XactAnalysis, and Xactimate. Collaborated with cross-functional teams in a SCRUM environment to address client issues and enhance the customer experience.</p>
+                        <p class="text-gray-500">Selected to identify and meet customer needs and assist customers in utilizing specific features in the software, including DASH, Pro-Assist, Luxor XactAnalysis, and Xactimate. Collaborated with cross-functional teams in a SCRUM environment to address client issues and enhance the customer experience. Effectively handled significant customer inquries and concerns, promptly submitting tickets to the development team via Jira for resolution or updates. </p>
                 </div>
                 <!-- <div class="flex mx-auto justify-center md:justify-start py-2"><a href="/about" class="about_button">Resume</a></div> -->
         </div>
@@ -103,7 +106,42 @@
 </section>
 
 <!-- Skills -->
-<section class="resume"></section>
+<!-- Expertise -->
+<section class="max-w-7xl mx-auto rounded-3xl shadow-lg p-5 my-8 bg-white grid grid-cols-1">
+        <div class="my-5">
+                <h2 class="text-3xl font-bold mb-2 underline">Technical Skills</h2>
+        </div>
+        <div class="grid grid-cols-1 md:grid-cols-4 ">
+                <ul class="space-y-2">
+                        <li> - Microsoft Office</li>
+                        <li> - Microsoft Dynamics</li>
+                        <li> - Power BI</li>
+                        <li> - Salesforce</li>
+                        <li class="pb-2 md:mb-0"> - Jira</li>
+                </ul>
+                <ul class="space-y-2">
+                        <li> - Wordpress </li>
+                        <li> - HTML, CSS</li>
+                        <li> - JavaScript</li>
+                        <li> - Python</li>
+                        <li class="pb-2 md:mb-0"> - Golang</li>
+                </ul>
+                <ul class="space-y-2">
+                        <li> - React.js</li>
+                        <li> - Next.js</li>
+                        <li> - Svelte.js</li>
+                        <li> - Bootstrap</li>
+                        <li class="pb-2 md:mb-0"> - Tailwind CSS</li>
+                </ul>
+                <ul class="space-y-2">
+                        <li> - Adobe Creative Suite</li>
+                        <li> - Figma</li>
+                        <li> - Canva</li>
+                        <li> - Windows</li>
+                        <li> - Macintosh</li>
+                </ul>
+        </div>
+</section>
 
 
 <!-- Awards/Organizations -->
