0ceb9ca2bf6c11d9b99bb05fd13ae09ac2249a9f
Richard Andrew Long <id-dev@Richards-MacBook-Air.local>
2024-03-19 09:34:30 -0500

    additions to branding section

--- src/lib/home/brands.svelte
+++ src/lib/home/brands.svelte
@@ -8,6 +8,15 @@
         <div class="brands_div">
             
                   <div class="brands">
+
+                    <div class="brands_bg group"
+                    style="background-image:url('/brands/showerpower.webp')">
+                    <div
+                  class="brands_txt_div">
+                  <h2 class="mt-4 text-xl font-semibold text-white capitalize">Shower Power</h2>
+                  <p class="mt-2 text-lg tracking-wider text-orange-500 uppercase ">Branding & Web Development</p>
+                 </div>
+                </div>
                       <div class="brands_bg group"
                           style="background-image:url('/brands/fat8.webp')">
                           <div
@@ -59,6 +68,15 @@
                   class="brands_txt_div">
                   <h2 class="mt-4 text-xl font-semibold text-white capitalize">Finder's Guide</h2>
                   <p class="mt-2 text-lg tracking-wider text-orange-500 uppercase ">Branding & Web Development</p>
+              </div>
+            </div>
+
+            <div class="brands_bg group"
+              style="background-image:url('/brands/trippy.webp')">
+              <div
+                  class="brands_txt_div">
+                  <h2 class="mt-4 text-xl font-semibold text-white capitalize">Trippy Booth</h2>
+                  <p class="mt-2 text-lg tracking-wider text-orange-500 uppercase ">Branding</p>
               </div>
             </div>
                     
